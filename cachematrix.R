10cf6f94eefc38d5db3b7c192ae3310c23d7dbfb
gpmapua19 <91687007+gpmapua19@users.noreply.github.com>
2021-10-01 16:31:00 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -10,9 +10,10 @@ makeCacheMatrix <- function(x = matrix()) {
                 inv <<- NULL
         }
         get <- function() {x}
-        setInverse <- function(inverse
-                
-
+        setInverse <- function(inverse) {inv <<- inverse}
+        getInverse <- function(){inv}
+        list(set = set, get = get, setInverse = setInverse,
+             getInverse - getInverse)
 }
 
 
